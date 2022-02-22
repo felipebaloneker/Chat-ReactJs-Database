8c077d5ff1a85e0b530ab6e80bed84cffe05a041
hunter <hunter>
2022-02-21 23:21:50 -0300

    updated

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     "type":"postgres",
     "url":process.env.DATABASE_URL,
-    "migrations":["dis/database/migrations/*.js"],
+    "migrations":["dist/database/migrations/*.js"],
     "entities":["dist/entities/*.js"],
     "cli":{
         "migrationsDir":"src/database/migrations",
