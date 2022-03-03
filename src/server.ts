d6217013ea03427b1d4b17b71c96a5b59d6b569b
hunter <hunter>
2022-03-02 22:18:51 -0300

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,17 @@
 import 'reflect-metadata';
-import "./database"
-import express from 'express'
-import cors from 'cors'
 import { routes } from "./routes"
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 3000;
 require('dotenv').config()
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
+import "./database"
+
 app.use(routes);
 
 app.listen(PORT,() =>{console.log(`Server is running on port:${PORT}`)});
