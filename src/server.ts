c3aad7e325ba7cd46265ea3ec938a959dd0622a6
hunter <hunter>
2022-03-02 22:14:09 -0300

    updated

--- src/server.ts
+++ src/server.ts
@@ -8,10 +8,13 @@ const PORT = process.env.PORT || 3000;
 require('dotenv').config()
 
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(routes);
 
 app.listen(PORT,() =>{console.log(`Server is running on port:${PORT}`)});
