1797d0ea808e65551d9901d57923ae54ac15c680
hunter <hunter>
2022-03-09 20:49:58 -0300

    updated

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,33 @@
+import express, { Request, Response, NextFunction } from 'express';
 import 'reflect-metadata';
-import { routes } from "./routes"
-const express = require('express');
+const dotenv = require('dotenv')
+const routes = require('./routes')
 const cors = require('cors');
 const app = express();
-const dotenv = require('dotenv')
 const PORT = process.env.PORT || 3000;
 
-dotenv.config()
+import './database'
 
+dotenv.config()
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 import "./database"
 
+//tratando errors
+
 app.use(routes);
 
-app.listen(PORT,() =>{console.log(`Server is running on port:${PORT}`)});
+app.use((err:Error, request:Request, response:Response, next: NextFunction)=>{
+    if(err instanceof Error){
+        return response.status(400).json({
+            error:err.message
+        })
+    }
+    return response.status(500).json({
+        status:'error',
+        message:'Internal Server Error'
+    })
+})
+app.listen(PORT,() =>{console.log(`Server is running`)});
